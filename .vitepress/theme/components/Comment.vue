5ee47e6cdc7ac168647c0ca67de50b4fa04fe2c4
mancuoj <1337381768@qq.com>
2023-03-29 16:25:27 +0800

    giscus

---
+++ .vitepress/theme/components/Comment.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import Giscus from '@giscus/vue'
+import { useData, useRoute } from 'vitepress'
+import { computed } from 'vue'
+
+const { isDark } = useData()
+const route = useRoute()
+const theme = computed(() => (isDark.value ? 'transparent_dark' : 'light_tritanopia'))
+</script>
+
+<template>
+  <div id="giscus">
+    <Giscus
+      :term="route.path"
+      id="comments"
+      repo="mancuoj/vitepress-blog"
+      repoId="R_kgDOJPXGYg"
+      category="Announcements"
+      categoryId="DIC_kwDOJPXGYs4CVYkY"
+      mapping="pathname"
+      strict="1"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      lang="zh-CN"
+      :theme="theme"
+      async
+    />
+  </div>
+</template>
+
+<style scoped>
+#giscus {
+  margin-top: 3rem;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,13 +2,15 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import Blog from './components/Blog.vue'
+import Comment from './components/Comment.vue'
 import './style.css'
 
 export default {
   ...Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
-      'home-features-after': () => h(Blog)
+      'home-features-after': () => h(Blog),
+      'doc-after': () => h(Comment)
     })
   },
   enhanceApp({ app, router, siteData }) {}
